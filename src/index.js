e82706592c8b0b6a2f9c212f6c3db66504d26caa
moralesvictorr <vmmorales@unicauca.edu.co>
2022-12-22 11:36:34 -0500

    feat: adding js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// Sprint 1 Makaia - Desarrollado por Víctor Manuel Morales Hoyos
+import "./styles/style.css";
+
+const DATE_TARGET = new Date('04/13/2023 0:01 AM');
+const SPAN_DAYS = document.querySelector('span#days');
+const SPAN_HOURS = document.querySelector('span#hours');
+const SPAN_MINUTES = document.querySelector('span#minutes');
+const SPAN_SECONDS = document.querySelector('span#seconds');
+
+// Milliseconds for the calculations
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+function updateCountdown() {
+  // Calcs
+  const NOW = new Date() 
+  const DURATION = DATE_TARGET - NOW;
+  const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY).toString().padStart(2, '0');/* El pad start es para mostrar cero a la izquierda */
+  const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR).toString().padStart(2, '0');
+  const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE).toString().padStart(2, '0');
+  const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND).toString().padStart(2, '0');
+
+  // Render
+  SPAN_DAYS.textContent = REMAINING_DAYS;
+  SPAN_HOURS.textContent = REMAINING_HOURS;
+  SPAN_MINUTES.textContent = REMAINING_MINUTES;
+  SPAN_SECONDS.textContent = REMAINING_SECONDS;
+}
+
+updateCountdown();
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
